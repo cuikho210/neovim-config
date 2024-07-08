64cfc534fc318b25e1116f8550287bd67a2f33a6
cuikho210 <tonykms210@gmail.com>
2024-07-09 02:25:17 +0700

    update move tab key binding

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,3 +8,21 @@ require("bufferline").setup {
 		diagnostics = "coc",
 	},
 }
+
+function TabMoveNext()
+  local current_tab = vim.fn.tabpagenr()
+  local total_tabs = vim.fn.tabpagenr('$')
+  if current_tab < total_tabs then
+    vim.cmd('tabm +')
+  end
+end
+
+function TabMovePrevious()
+  local current_tab = vim.fn.tabpagenr()
+  if current_tab > 1 then
+    vim.cmd('tabm -')
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<C-S-PageUp>', ':lua TabMoveNext()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-S-PageDown>', ':lua TabMovePrevious()<CR>', { noremap = true, silent = true })
